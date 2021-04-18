43dc280d3b329f07a7a956fb4833384eab6f21d4
ChaseChilders <childe03@nsuok.edu>
2021-04-17 23:42:01 -0500

    Commit

---
+++ NavMeshComponents-master/Assets/Examples/Scripts/Collector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collector : MonoBehaviour
+{
+    public Text scoreText;
+    
+    private int count = 0;
+    
+    void Update()
+    {
+        scoreText.text = "Score: " + count;
+    }    
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            count += 10;
+            
+        }
+    }
+}
---
+++ NavMeshComponents-master/Assets/Examples/Scripts/OgreMove.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityEngine.AI;
+using System.Collections;
+public class OgreMove : MonoBehaviour
+{
+    
+    public NavMeshAgent enemyAgent;
+    public GameObject player;
+    public Text lifeText;
+    private int life = 3;
+
+    private void Start()
+    {
+        enemyAgent = GetComponent<NavMeshAgent>();
+        enemyAgent.updateRotation = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        enemyAgent.SetDestination(player.transform.position);
+        lifeText.text = "Lives: " + life;
+    }
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            life -= 1;
+            gameObject.GetComponent<Collider>().enabled = false;
+            yield return new WaitForSeconds(1);
+            gameObject.GetComponent<Collider>().enabled = true;
+            Debug.Log("Oh No!");
+
+        }
+    }
+}
+
